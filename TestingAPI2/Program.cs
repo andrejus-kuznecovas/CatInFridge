3490d6323cff427781900627557fe3d426f20d40
Kuznecov Andrej <andrej.kuznecov@mif.stud.vu.lt>
2017-09-19 21:22:11 +0300

    Adds methods to put string into files and a unit test

---
+++ TestingAPI2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+using System.IO;
+
+namespace TestingAPI2
+{
+    public class Program
+    {
+        public string imageLoc = "D:\\image05.jpg";
+        public string outputLoc1 = "D:\\output\\check.txt";
+        public string outputLoc2 = "D:\\output\\check2.txt";
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.ImageToText();
+        }
+
+        public void ImageToText()
+        {
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.Lithuanian);
+                string plainText = api.GetTextFromImage(filename: @imageLoc);
+                Console.WriteLine(plainText);
+                //ToFile(plainText);
+                //ToFileNewLine(plainText);
+                Console.Read();
+            }
+        }
+
+        //šitas medotas perrašo tekstinį failą kiekvieną kartą
+        public void ToFile(string plainText)
+        {
+            System.IO.File.WriteAllText(@outputLoc1, plainText);
+        }
+
+        //šitas metodas įrašo stringą į naują eilutę kiekvieną kartą (neperrašo)
+        public void ToFileNewLine(string plainText)
+        {
+            StreamWriter file1 = new StreamWriter(outputLoc2, true);
+            file1.WriteLine(plainText);
+            file1.Close();
+        }
+        
+    }
+}
---
+++ TestingAPI2Tests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingAPI2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingAPI2.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void ToFileTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
