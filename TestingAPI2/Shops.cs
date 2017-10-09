edb4aa6c9c497515bb08981b4eb1c95c324fd74e
Kuznecov Andrej <andrej.kuznecov@mif.stud.vu.lt>
2017-10-09 13:59:29 +0300

    solving conflicts

---
+++ TestingAPI2/Shops.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingAPI2
+{
+    public enum Shop
+    {
+        IKI, MAXIMA, NORFA, RIMI, UNRECOGNIZED
+    };
+}
