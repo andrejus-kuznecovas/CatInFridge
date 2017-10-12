1669d1d085c9cd1dbed2f9bf599e4f46932e0927
Lukas <lukasjurevicius1@gmail.com>
2017-10-12 23:55:39 +0300

    Expanding Item.cs and SearchEngine.cs classes
    Search Engine can now:
    - load items into an xml file,
    - can find items of a given category and compare their price to the price of  scanned item;
    
    Items.cs:
    -has new fields (productName, price, category and shop);
    -overridden ToString() method;

---
+++ TestingAPI2/Item.cs
@@ -0,0 +1,52 @@
+﻿namespace TestingAPI2
+{
+    public enum Category
+    {
+        MEAT, VEGETABLES_FRUITS, DAIRY, DRINKS, ALCOHOL, BREAD, SWEETS, OTHER, UNRECOGNIZED
+
+    }
+    public enum Shop
+    {
+        MAXIMA, IKI, RIMI, NORFA, OTHER, UNRECOGNIZED
+
+    }
+    class Item
+    {
+        private string productName;
+        private double price;
+        private Category category;
+        private Shop shop;
+
+        /** If one (or both) of the enums are not passed in constructor, their are set to 'UNRECOGNIZED'*/
+
+        /* Constructor #1: both enums are set to 'UNRECOGNIZED'*/
+        public Item(string productName, double price)
+        {
+            this.Price = price;
+            this.ProductName = productName;
+            this.Category = Category.UNRECOGNIZED;
+            this.Shop = Shop.UNRECOGNIZED;
+        }
+
+
+        /* Constructor #2: category is recognized, shop is unrecognized*/
+        public Item(string productName, double price, string category)
+        {
+            this.Price = price;
+            this.ProductName = productName;
+            this.Category = Category.UNRECOGNIZED;
+            this.Shop = Shop.UNRECOGNIZED;
+        }
+
+        public string ProductName { get => productName; set => productName = value; }
+        public double Price { get => price; set => price = value; }
+        public Category Category { get => category; set => category = value; }
+        public Shop Shop { get => shop; set => shop = value; }
+
+        /* overridding ToString method to check the class fields more easily*/
+        public override string ToString()
+        {
+            return "Name: " + ProductName + ", price: " + Price + ", category: " + Category + ", shop: " + Shop + ".";
+        }
+    }
+}
--- TestingAPI2/SearchEngine.cs
+++ TestingAPI2/SearchEngine.cs
@@ -1,18 +1,138 @@
 ﻿using System;
+using TestingAPI2;
+using System.Xml;
 
-namespace TestingAPI2
+namespace ConsoleApp2
 {
-    public class SearchEngine
+    class SearchEngine
     {
-        
-        public SearchEngine(Item item, String XMLloc)
+        static void Main(string[] args)
         {
-            //default constructor - does nothing
+            Item item = new Item("Pzpz", 3.14);
+            Console.WriteLine(item);
+
+            /** XML TEST*/
+            FindByCategory(item, "D:\\products.xml");
+            Console.Read();
+        }
+        public static void LoadIntoXML(Item item, string XmlLoc)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            /* Check if location XmlLoc is empty or null*/
+            if (String.IsNullOrEmpty(XmlLoc)) { XmlLoc = "D:\\products.xml"; }
+
+            /* Load item info into xml file */
+            doc.LoadXml(("<Product>" +
+                         "<Price>" + item.Price + "</Price>" +
+                         "<Shop>" + item.Shop + "</Shop>" +
+                         "<Name>" + item.ProductName + "</Name>" +
+                         "<Category>" + item.Category + "</Category>" +
+                         "</Product>"));
+            //Save the document to a file.  
+            doc.Save("D:\\products.xml");
         }
 
+        public static void FindByCategory(Item item, string Xmloc)
+        {
+            XmlTextReader textReader = new XmlTextReader(Xmloc);
+            string _price = null; // extracted price
+            string _category = null; // extracted category
+            string _name = null; // extracted name
+            string _shop = null; // extracted shop
 
+            while (textReader.Read())
+            {
+                XmlNodeType nType = textReader.NodeType;
+
+                /* if node type is an element */
+                if (nType == XmlNodeType.Element)
+                {
+                    switch (textReader.Name.ToString())
+                    {
+                        case "Price":
+                            /* Element named <Price> found here*/
+                            textReader.Read();
+                            if (textReader.NodeType == XmlNodeType.Text)
+                            {
+                                /* Extract price from .xml file */
+                                _price = textReader.ReadString();
+                            }
+                            break;
+
+                        case "Shop":
+                            /* Element named <Shop> found here*/
+                            textReader.Read();
+                            if (textReader.NodeType == XmlNodeType.Text)
+                            {
+                                /* Extract shop from .xml file */
+                                _shop = textReader.ReadString();
+                            }
+                            break;
+                        case "Name":
+                            /* Element named <Name> found here*/
+                            textReader.Read();
+                            if (textReader.NodeType == XmlNodeType.Text)
+                            {
+                                /* Extract shop from .xml file */
+                                _name = textReader.ReadString();
+
+                            }
+                            break;
+                        case "Category":
+                            /* Element named <Category> found here*/
+                            textReader.Read();
+                            if (textReader.NodeType == XmlNodeType.Text)
+                            {
+                                /* Extract category from .xml file */
+                                _category = textReader.ReadString();
+                                /* Check if category matches */
+                                if (_category.Equals(item.Category.ToString()))
+                                {
+                                    /* Category matches; Element text found here*/
+                                    Console.WriteLine("Product Found");
+                                    Console.WriteLine("Name: " + _name +
+                                                      ", price: " + _price +
+                                                      ", shop: " + _shop +
+                                                      ", category: " + _category + ".");
+
+                                    double storedPrice = XmlConvert.ToDouble(_price);
+                                    // Do price checks here
+                                    if (item.Price > storedPrice)
+                                    {
+                                        //Product in the database is cheaper
+                                        Console.WriteLine("Product in the database is cheaper");
+                                    }
+                                    else if (item.Price < storedPrice)
+                                    {
+                                        //Scanned item is cheaper
+                                        Console.WriteLine("Scanned item is cheaper");
+                                    }
+                                    else
+                                    {
+                                        //Scanned item is of equal value to found item
+                                        Console.WriteLine("Scanned item is of equal value to found item");
+                                    }
+                                    Console.WriteLine("***********************************");
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+
+
+
+
+
+                    }
+                }
+
+
+            }
+        }
+        public static void FindByName(Item item, string Xmloc)
+        {
+            /* A function to find elements with the name exact to item's name */
+        }
     }
-
-        
 }
-
