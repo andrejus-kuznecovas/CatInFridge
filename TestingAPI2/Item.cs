daee72433710905fdf2b8212dbff7efa287d689d
Lukas <lukasjurevicius1@gmail.com>
2017-10-19 19:42:09 +0300

    Expanded SearchEngine, Item classes
    - Added Search method to  SearchEngine class
    - Added FixString Method to SearchEngine class
    - Added more constructors and enum parsing to Item.cs class

--- TestingAPI2/Item.cs
+++ TestingAPI2/Item.cs
@@ -1,4 +1,10 @@
-﻿namespace TestingAPI2
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
 {
     public enum Category
     {
@@ -28,14 +34,26 @@
             this.Shop = Shop.UNRECOGNIZED;
         }
 
-
-        /* Constructor #2: category is recognized, shop is unrecognized*/
+        /* Constructor #2: category is set according to parameter 'category', shop is unrecognized*/
         public Item(string productName, double price, string category)
         {
             this.Price = price;
             this.ProductName = productName;
-            this.Category = Category.UNRECOGNIZED;
             this.Shop = Shop.UNRECOGNIZED;
+            Enum.TryParse(category, out Category cat);
+            this.Category = cat;
+
+        }
+
+        /* Constructor #3: category is set to parameter 'category', shop is set to parameter 'shop'*/
+        public Item(string productName, double price, string category, string shop)
+        {
+            this.Price = price;
+            this.ProductName = productName;
+            Enum.TryParse(category, out Category cat);
+            this.Category = cat;
+            Enum.TryParse(shop, out Shop enumshop);
+            this.Shop = enumshop;
         }
 
         public string ProductName { get => productName; set => productName = value; }
--- TestingAPI2/SearchEngine.cs
+++ TestingAPI2/SearchEngine.cs
@@ -1,18 +1,34 @@
 ﻿using System;
-using TestingAPI2;
 using System.Xml;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace ConsoleApp2
+namespace ConsoleApp1
 {
     class SearchEngine
     {
         static void Main(string[] args)
         {
-            Item item = new Item("Pzpz", 3.14);
-            Console.WriteLine(item);
+            Item item = new Item("Ledai plombyras \"Lyginamasis\"", 3.14, "DAIRY", "RIMI");
+            ArrayList items = new ArrayList();
+            items.Add(new Item("Ledai Plombyras \"Svajonė\"", 3.10, "DAIRY", "IKI"));
+            items.Add(new Item("Ledai Plombyras \"Realybė\"", 3.18, "DAIRY", "RIMI"));
+            items.Add(new Item("Ledai Plombyras \"Feikinis\"", 3.18, "Other", "RIMI"));
+            items.Add(new Item("Dešra \"Medžiotojų\"", 0.99, "MEAT", "RIMI"));
+            items.Add(new Item("Alus \"Utenos\"", 4.10, "ALCOHOL", "OTHER"));
+            items.Add(new Item("Pienas \"Dvaro\"", 2.04, "DAIRY", "OTHER"));
+            //Console.WriteLine(item);
+            foreach (Item loopItem in items)
+            {
+                //  Console.WriteLine(loopItem);
+            }
 
-            /** XML TEST*/
-            FindByCategory(item, "D:\\products.xml");
+            Search(item.ProductName, items, item.Category.ToString());
+            //FindByCategory(item, @"C:\xml\products.xml");
+
             Console.Read();
         }
         public static void LoadIntoXML(Item item, string XmlLoc)
@@ -20,7 +36,7 @@ namespace ConsoleApp2
             XmlDocument doc = new XmlDocument();
 
             /* Check if location XmlLoc is empty or null*/
-            if (String.IsNullOrEmpty(XmlLoc)) { XmlLoc = "D:\\products.xml"; }
+            if (String.IsNullOrEmpty(XmlLoc)) { XmlLoc = "C:\\products.xml"; }
 
             /* Load item info into xml file */
             doc.LoadXml(("<Product>" +
@@ -30,7 +46,7 @@ namespace ConsoleApp2
                          "<Category>" + item.Category + "</Category>" +
                          "</Product>"));
             //Save the document to a file.  
-            doc.Save("D:\\products.xml");
+            doc.Save("C:\\xml\\products.xml");
         }
 
         public static void FindByCategory(Item item, string Xmloc)
@@ -40,6 +56,7 @@ namespace ConsoleApp2
             string _category = null; // extracted category
             string _name = null; // extracted name
             string _shop = null; // extracted shop
+            bool categoryMatches = false;
 
             while (textReader.Read())
             {
@@ -76,7 +93,6 @@ namespace ConsoleApp2
                             {
                                 /* Extract shop from .xml file */
                                 _name = textReader.ReadString();
-
                             }
                             break;
                         case "Category":
@@ -89,50 +105,145 @@ namespace ConsoleApp2
                                 /* Check if category matches */
                                 if (_category.Equals(item.Category.ToString()))
                                 {
+                                    categoryMatches = true;
                                     /* Category matches; Element text found here*/
-                                    Console.WriteLine("Product Found");
-                                    Console.WriteLine("Name: " + _name +
-                                                      ", price: " + _price +
-                                                      ", shop: " + _shop +
-                                                      ", category: " + _category + ".");
-
-                                    double storedPrice = XmlConvert.ToDouble(_price);
-                                    // Do price checks here
-                                    if (item.Price > storedPrice)
-                                    {
-                                        //Product in the database is cheaper
-                                        Console.WriteLine("Product in the database is cheaper");
-                                    }
-                                    else if (item.Price < storedPrice)
-                                    {
-                                        //Scanned item is cheaper
-                                        Console.WriteLine("Scanned item is cheaper");
-                                    }
-                                    else
-                                    {
-                                        //Scanned item is of equal value to found item
-                                        Console.WriteLine("Scanned item is of equal value to found item");
-                                    }
-                                    Console.WriteLine("***********************************");
                                 }
                             }
                             break;
                         default:
                             break;
-
-
-
-
-
                     }
                 }
 
+                else if (nType == XmlNodeType.EndElement)
+                {
+                    if (textReader.Name.ToString().Equals("Product"))
+                    {
+                        if (categoryMatches)
+                        {
+                            Console.WriteLine("Product Found");
+                            Console.WriteLine("Name: " + _name +
+                                              ", price: " + _price +
+                                              ", shop: " + _shop +
+                                              ", category: " + _category + ".");
+
+                            Regex.Replace(_price, @"\s+", "");
+                            double storedPrice = XmlConvert.ToDouble(_price);
+                            // Do price checks here
+                            if (item.Price > storedPrice)
+                            {
+                                //Product in the database is cheaper
+                                Console.WriteLine("Product in the database is cheaper");
+                            }
+                            else if (item.Price < storedPrice)
+                            {
+                                //Scanned item is cheaper
+                                Console.WriteLine("Scanned item is cheaper");
+                            }
+                            else
+                            {
+                                //Scanned item is of equal value to found item
+                                Console.WriteLine("Scanned item is of equal value to found item");
+                            }
+                            Console.WriteLine("***********************************");
+
+                            //reset bool categoryMatches
+                            categoryMatches = false;
+                        }
+                    }
+                }
 
             }
         }
-        public static void FindByName(Item item, string Xmloc)
+
+        // A method to find the most similar items in the list according to the itemName
+        public static ArrayList Search(string itemName, ArrayList itemList, string category)
         {
-            /* A function to find elements with the name exact to item's name */
+
+            ArrayList results = new ArrayList(); //returned arraylist
+            Dictionary<string, int> itemDic = new Dictionary<string, int>(); //dictionary for mapping item names to points
+            StringBuilder keyword = new StringBuilder(); //used for building keywords (compare values)
+
+            //Fix itemName string (see fixStrings method)
+            itemName = FixStrings(itemName);
+
+            //Splitting itemName by words
+            char[] separators = { ' ' }; // separators for spliting itemName 
+            String[] itemNameSubs = itemName.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            //Select only items of the given category
+            var sameCategoryItems = from Item itemx in itemList
+                                    where itemx.Category.ToString() == category
+                                    select itemx;
+
+            foreach (Item it in sameCategoryItems)
+            {
+                //fix the arraylist's product names string (see fixStrings method)
+                it.ProductName = FixStrings(it.ProductName);
+
+                //filling the dictionary with existing item names, setting zero points to each
+                itemDic.Add(it.ProductName, 0);
+                //Console.WriteLine(it);
+            }
+
+            for (int i = 0; i < itemNameSubs.Length; i++)
+            {
+
+                //build the keyword
+                keyword.Append(itemNameSubs[i]);
+                //Console.WriteLine(keyword.ToString());
+                //Console.WriteLine("Now searching for the keyword: " + keyword.ToString() ); 
+
+                //Search for the keyword in the sameCategoryItems & assign points
+                foreach (Item it in sameCategoryItems)
+                {
+                    if (it.ProductName.Contains(keyword.ToString()))
+                    {
+                        //Assign a point
+                        itemDic[it.ProductName] += 1;
+                        //Console.WriteLine("Assigning a point to: '" + it.ProductName + "' points now: " + itemDic[it.ProductName]);
+                    }
+
+                }
+                //Add a space after the last word
+                keyword.Append(" ");
+                //Console.WriteLine("**********************************************");
+            }
+
+            // retrieving all keys with the maximum values
+            var keys = itemDic.Where(pair => pair.Value == itemDic.Values.Max()).Select(pair => pair.Key).ToArray(); // if you want an array of these keys
+            foreach (string key in keys)
+            {
+                //Console.WriteLine(key);
+                // search for product/item with the name equal to key
+                foreach (Item it in sameCategoryItems)
+                {
+                    if (it.ProductName == key)
+                    {
+                        results.Add(it);
+                    }
+                }
+            }
+            foreach (Item it in results)
+            {
+                // Console.WriteLine(it);
+            }
+            return results;
+        }
+
+        //Replace Lithuanian symbols with appropriate latin symbols, sets to lowercase*/
+        public static string FixStrings(string str)
+        {
+            str = str.ToLower();
+            str = Regex.Replace(str, "ą", "a");
+            str = Regex.Replace(str, "č", "c");
+            str = Regex.Replace(str, "ę", "e");
+            str = Regex.Replace(str, "ė", "e");
+            str = Regex.Replace(str, "į", "i");
+            str = Regex.Replace(str, "š", "s");
+            str = Regex.Replace(str, "ų", "u");
+            str = Regex.Replace(str, "ū", "u");
+            return str;
         }
     }
 }
