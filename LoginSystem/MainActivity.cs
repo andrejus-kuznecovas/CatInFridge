69c386114ba7281de953e9696598718e465507e4
Kuznecov Andrej <andrej.kuznecov@mif.stud.vu.lt>
2017-10-26 12:20:59 +0300

    update colors, fonts, add 2 styles

---
+++ LoginSystem/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Threading;
+
+namespace LoginSystem
+{
+    [Activity(Label = "LoginSystem", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button mBtnUpload;
+        private Button mBtnPhoto;
+        private ProgressBar mProgressBar;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mBtnUpload = FindViewById<Button>(Resource.Id.btnUpload);
+            mBtnPhoto = FindViewById<Button>(Resource.Id.btnPhoto);
+            mProgressBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+
+           
+        }
+
+  
+       
+        private void ActLikeARequest()
+        {
+            Thread.Sleep(3000);
+
+            RunOnUiThread(() => { mProgressBar.Visibility = ViewStates.Invisible; });
+            int x = Resource.Animation.slide_right;
+        }
+    }
+}
+
