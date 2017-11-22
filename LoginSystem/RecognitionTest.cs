7ee19d3368e2a4e3c39444a623f81462edca3031
Kuznecov Andrej <andrej.kuznecov@mif.stud.vu.lt>
2017-11-22 18:10:24 +0200

    Add test class for dependancy injection

---
+++ LoginSystem/RecognitionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using System.Threading.Tasks;
+using Tesseract.Droid;
+using Android.Graphics;
+using System.IO;
+
+namespace LoginSystem
+{
+    public class RecognitionTest : ITextRecognition
+    {
+        // Event to be called after the recognition is done
+        private EventHandler<OCRInfo> OnOCRCompleteTest;
+
+        public void AddOnCompleteHandler(EventHandler<OCRInfo> action)
+        {
+            OnOCRCompleteTest = action;
+        }
+
+
+        public void GetTextFromImage(Bitmap image)
+        {
+                
+        }
+    }
+ }
